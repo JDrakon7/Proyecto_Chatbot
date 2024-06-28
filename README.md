f26f491b36f05c3ee5130c7d59768cae6136595b
JuanDavidBarreraJaimesADSO <jdbarrera80@gmail.com>
2024-06-28 11:15:45 -0500

    Actualizacion vistas

---
+++ README.md
@@ -0,0 +1,10 @@
+### Acerca del Bot
+
+Masterbot es una chatbot creado principalmente como un proyecto de certificacion de la tecnologia en analisis y desarrollo de software ADSO
+el objetivo de este bot se basa principalmente en ofrecer un soporte dinamico en el ambito de la logica de programacion de python , enfatizando en las posibilidades y futuras especializaciones.
+
+### Requisitos de uso 
+* python 3.5+
+* pytorch 0.4.0
+* tqdm
+* nltk
--- assets/js/main.js.js
+++ assets/js/main.js.js
@@ -1,3 +1,16 @@
+
+const data = await response.json();
+const botMessage = data.response;
+
+const response = await fetch('/chat', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ message })
+});
+
+
 document.getElementById('send-btn').addEventListener('click', async () => {
     const input = document.getElementById('chat-input');
     const message = input.value;
@@ -6,16 +19,6 @@ document.getElementById('send-btn').addEventListener('click', async () => {
     const chatContainer = document.querySelector('.chat_container');
     chatContainer.innerHTML += `<div class="chat outgoing"><div class="chat-content"><p>${message}</p></div></div>`;
   
-    const response = await fetch('/chat', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ message })
-    });
-  
-    const data = await response.json();
-    const botMessage = data.response;
   
     chatContainer.innerHTML += `<div class="chat incoming"><div class="chat-content"><p>${botMessage}</p></div></div>`;
     input.value = '';
